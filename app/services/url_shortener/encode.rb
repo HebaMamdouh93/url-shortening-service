a051166e9017dfab7142e62d9904d59abd2854ed
Heba Mamdouh <hebamamdouh2016@gmail.com>
2025-07-05 16:58:38 +0300

    Add logic to encode URLs into short codes in the URL shortener service

---
+++ app/services/url_shortener/encode.rb
@@ -0,0 +1,31 @@
+module UrlShortener
+  class Encode
+    include CacheKeyHelper
+    def initialize(original_url:, encoder: Base62Encoder, cache: CacheService)
+      @original_url = original_url
+      @encoder = encoder
+      @cache = cache
+    end
+
+    def call
+      url = Url.find_by(original_url: @original_url)
+      return full_url(url.short_code) if url
+
+      ActiveRecord::Base.transaction do
+        url = Url.create!(original_url: @original_url)
+        code = @encoder.encode(url.id)
+        url.update!(short_code: code)
+        @cache.write(short_code_cache_key(code), url.original_url)
+        full_url(code)
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      raise ShortenerError, e.message
+    end
+
+    private
+
+    def full_url(code)
+      "#{Rails.application.credentials[:base_url]}/#{code}"
+    end
+  end
+end
