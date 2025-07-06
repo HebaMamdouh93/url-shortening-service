ccf29e37110a9d4a28f903b4ec15731ac4632602
Heba Mamdouh <hebamamdouh2016@gmail.com>
2025-07-06 12:41:39 +0300

    Handle exceptions for Base62Encoder service

--- lib/base62_encoder.rb
+++ lib/base62_encoder.rb
@@ -3,9 +3,13 @@ require 'base62-rb'
 module Base62Encoder
   def self.encode(id)
     Base62.encode(id)
+  rescue StandardError => e
+    raise ShortenerError, e.message 
   end
 
   def self.decode(code)
     Base62.decode(code)
+  rescue StandardError => e
+    raise ShortenerError, e.message  
   end
 end
