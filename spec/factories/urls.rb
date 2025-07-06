ef3e105802313d70d4e989fea26633210dc1a363
Heba Mamdouh <hebamamdouh2016@gmail.com>
2025-07-06 12:43:44 +0300

    Add with_short_code trait to urls factories

--- spec/factories/urls.rb
+++ spec/factories/urls.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :url do
     original_url { Faker::Internet.url }
     short_code { Faker::Alphanumeric.alpha(number: 6) }
+
+    trait :with_short_code do
+      after(:create) do |url|
+        code = Base62Encoder.encode(url.id)
+        url.update!(short_code: code)
+      end
+    end
   end
 end
